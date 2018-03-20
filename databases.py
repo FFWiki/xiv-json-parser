4bf8cdbbb487b17397a43486799ecc694eda4217
Aidan Backus <tenshi@localhost.localdomain>
2018-03-20 15:56:59 -0700

    More databases, more efficient compression

--- databases.py
+++ databases.py
@@ -1,7 +1,23 @@
 ENEMY_MAP_DATA_MAPS = {"placename_name": {}, "zone_name": {}}
+ITEM_ATTRIBUTES_PARAMS = {"name": {}, "value": {}, "value_hq": {}}
+LEVE_ITEMS = {"name": {}, "id": {}}
+
+ITEM_CLASSES = {} # TODO: Add all item classes.
 
 def recursive(args):
-    pass # TODO: This
+    d = {}
+    for i in range(32): # this constant could be wrong?
+        d[i] = args
+    return d
+
+def items(args):
+    d = {}
+    for cls in ITEM_CLASSES:
+        sd = {}
+        for subcls in cls:
+            sd[subcls] = recursive(args)
+        d[cls] = sd
+    return d
 
 table = {
 	"achievement": {
@@ -60,11 +76,82 @@ table = {
                 'name_fr': {},
                 'name_de': {},
                 'map_data': {
-                    # 'maps': recursive(ENEMY_MAP_DATA_MAPS)
-                    # TODO: This
+                    'maps': recursive(ENEMY_MAP_DATA_MAPS),
                     'stats': {'hpAvg': {}, 'hpMax': {}, 'hpMin': {}, 'levelMax': {}, 'levelMin': {}, 'mpAvg': {}, 'mpMax': {}, 'mpMin': {}},
                 },
                 'patch': {'name': {}, 'number': {}},
+            },
+        },
+        "fate": {
+            "url": "https://api.xivdb.com/fate",
+            "keys": {
+                'name': {},
+                'name_ja': {},
+                'name_fr': {},
+                'name_de': {},
+                'help_en': {},
+                'class_level': {},
+                'class_level_max': {},
+                'map': {'placename': {'name': {}}},
+                'patch': {'name': {}, 'number': {}},
+            },
+        },
+        "instance": {
+            "url": "https://api.xivdb.com/instance",
+            "keys": {
+                'name': {},
+                'name_ja': {},
+                'name_fr': {},
+                'name_de': {},
+                'help_en': {},
+                'level': {},
+                'level_sync': {},
+                'time_limit': {},
+                'tanks_per_party': {},
+                'healers_per_party': {},
+                'dps_per_party': {},
+                'item_level': {},
+                'item_level_sync': {},
+                'content_name': {},
+                'patch': {'name': {}, 'number': {}},
+            },
+        },
+        "item": {
+            "url": "https://api.xivdb.com/item",
+            "keys": {
+                'name': {},
+                'name_ja': {},
+                'name_fr': {},
+                'name_de': {},
+                'help_en': {},
+                'classjob_category': {},
+                'level_equip': {},
+                'level_item': {},
+                'category_name': {},
+                'attributes_base': {'auto_attack': {}, 'auto_attack_hq': {}, 'block_rate': {}, 'block_rate_hq': {}, 'block_strength': {}, 'block_strength_hq': {},
+                    'damage': {}, 'damage_hq': {}, 'defense': {}, 'defense_hq': {}, 'delay': {}, 'delay_hq': {}, 'magic_damage': {}, 'magic_damage_hq': {},
+                    'magic_defense': {}, 'magic_defense_hq': {}},
+                'attributes_params': recursive(ITEM_ATTRIBUTES_PARAMS),
+                'patch': {'name': {}, 'number': {}},
+            },
+        },
+        "leve": {
+            "url": "https://api.xivdb.com/leve",
+            "keys": {
+                'name': {},
+                'name_ja': {},
+                'name_fr': {},
+                'name_de': {},
+                'help_en': {},
+                'gil_reward': {},
+                'exp_reward': {},
+                'leve_client': {},
+                'class_level': {},
+                'classjob_category': {},
+                'time_limit': {},
+                'placename': {'name': {}},
+                'patch': {'name': {}, 'number': {}},
+                'items': items(LEVE_ITEMS),
             },
         },
 }
--- json_parser.py
+++ json_parser.py
@@ -17,24 +17,29 @@ def recover_ids(url):
 
 def tostr(v, keys):
     if isinstance(v, bool):
-        return (v and 1) or "'&mdash;'"
+        return (v and 1) or None
     elif isinstance(v, int) or isinstance(v, float):
+        if v == 0:
+            return None
         return str(v)
     elif isinstance(v, dict):
         return read_table(v, keys)
     elif isinstance(v, str):
-        return "'" + v + "'"
+        return "[[" + v + "]]"
     elif v is None or v == []:
-        return "'&mdash;'"
+        return None
     else:
         print("don't know what to do with")
         print(str(v))
+        return None
 
 def read_table(v, keys):
     lua = "{"
     for k in keys:
         if k in v:
-            lua += "['" + k + "']=" + tostr(v[k], keys[k]) + ","
+            s = tostr(v[k], keys[k])
+            if s is not None:
+                lua += k + "=" + s + ","
     lua += "}"
     return lua
 
@@ -47,13 +52,15 @@ def parse_run(url, keys):
     for uid in ids:
         if DEBUG and debug_halt:
             break
-        debug_halt = True
         if uid % 100 == 0:
             print("\t\tCurrently running UID " + str(uid))
         uid = str(uid)
+
         v = decode(url + "/" + uid)
         if 'is_in_game' in v and v['is_in_game'] == 0:
             continue
+        debug_halt = True
+        
         index += "['" + v['name'] + "']="+uid+","
         lua += "[" + uid + "]=" + read_table(v, keys) + ","
     lua += "},index={" + index + "}}"
