fc2d45550dc4836d504c6e04b288986dc4d60392
Aidan Backus <tenshi@localhost.localdomain>
2018-03-29 13:30:24 -0700

    Some more databases

--- databases.py
+++ databases.py
@@ -1,12 +1,20 @@
 ENEMY_MAP_DATA_MAPS = {"placename_name": {}, "zone_name": {}}
 ITEM_ATTRIBUTES_PARAMS = {"name": {}, "value": {}, "value_hq": {}}
 LEVE_ITEMS = {"name": {}, "id": {}}
+PLACENAME_ENEMIES = {"name": {}, positions: recursive(PLACENAME_ENEMIES_POSITIONS)}
+PLACENAME_ENEMIES_POSITIONS = {"hp": {}, "level": {}, "x": {}, "y": {}}
+PLACENAME_INSTANCES = {"name": {}, "level": {}, "content_type": {}, "help": {}}
+PLACENAME_NPCS = {"name": {}, "position": {"x": {}, "y": {}}}
+QUEST_TEXT = {"text": {}}
+TITLE_ACHIEVEMENTS = {"name": {}}
+EXPTABLE = {"exp": {}}
 
+QUESTS = {"name": {}, "genre_name": {}, "npc_name": {}, "patch": {"name": {}, "number": {}}}
 ITEM_CLASSES = {} # TODO: Add all item classes.
 
 def recursive(args):
     d = {}
-    for i in range(32): # this constant could be wrong?
+    for i in range(256): # this constant could be wrong?
         d[i] = args
     return d
 
@@ -20,6 +28,7 @@ def items(args):
     return d
 
 table = {
+        # TODO: Gathering, crafting
 	"achievement": {
 		"url": "https://api.xivdb.com/achievement",
 		"keys": {
@@ -151,9 +160,119 @@ table = {
                 'time_limit': {},
                 'placename': {'name': {}},
                 'patch': {'name': {}, 'number': {}},
-                'items': items(LEVE_ITEMS),
+                #'items': items(LEVE_ITEMS),
+                #TODO: This
             },
         },
+        "minion": {
+            "url": "https://api.xivdb.com/minion",
+            "keys": {
+                'name': {},
+                'name_ja': {},
+                'name_fr': {},
+                'name_de': {},
+                'info1': {},
+                'info2': {},
+                'summon': {},
+                'behavior': {},
+                'action': {},
+                'attack': {},
+                'cost': {},
+                'hp': {},
+                'defense': {},
+                'skill_cost': {},
+                'speed': {},
+                'patch': {'name': {}, 'number': {}},
+            },
+        },
+        "npc": {
+            "norepeat": True, #TODO: This
+            "url": "https://api.xivdb.com/npc",
+            "keys": {
+                'name': {},
+                'name_de': {},
+                'name_jp': {},
+                'name_fr': {},
+                'title': {},
+                'title_de': {},
+                'title_fr': {},
+                'title_jp': {},
+                'patch': {'name': {}, 'number': {}},
+            },
+        },
+        "placename": {
+            "url": "https://api.xivdb.com/placename",
+            "keys": {
+                'name': {},
+                'name_de': {},
+                'name_jp': {},
+                'name_fr': {},
+                'enemies': recursive(PLACENAME_ENEMIES),
+                'instances': recursive(PLACENAME_INSTANCES),
+                'npcs': recursive(PLACENAME_NPCS),
+                'quests': recursive(QUESTS),
+                'patch': {'name': {}, 'number': {}},
+            },
+        },
+        "quest": {
+            "url": "https://api.xivdb.com/quest",
+            "keys": {
+                'category_name': {},
+                'exp_reward': {},
+                'genre_name': {},
+                'gil_reward': {},
+                'name': {},
+                'name_jp': {},
+                'name_fr': {},
+                'name_de': {},
+                'npc_start': {'name': {}},
+                'npc_end': {'name': {}},
+                'patch': {'name': {}, 'number': {}},
+                'pre_quests': recursive(QUESTS),
+                'post_quests': recursive(QUESTS),
+                'text': {'journal': recursive(QUEST_TEXT), 'todo': recursive(QUEST_TEXT)},
+            },
+        },
+        "status": {
+            "url": "https://api.xivdb.com/status",
+            "keys": {
+                'name': {},
+                'name_de': {},
+                'name_jp': {},
+                'name_fr': {},
+                'help': {},
+                'patch': {'name': {}, 'number': {}},
+            },
+        },
+        "title": {
+            "url": "https://api.xivdb.com/title",
+            "keys": {
+                'name': {},
+                'name_de': {},
+                'name_jp': {},
+                'name_fr': {},
+                'name_female': {},
+                'name_female_de': {},
+                'name_female_jp': {},
+                'name_female_fr': {},
+                'achivements': recursive(TITLE_ACHIEVEMENTS),
+                'patch': {'name': {}, 'number': {}},
+            },
+        },
+        "weather": {
+            "url": "https://api.xivdb.com/weather",
+            "keys": {
+                'name': {},
+                'name_de': {},
+                'name_jp': {},
+                'name_fr': {},
+            },
+        },
+        "exptable": {
+            "url": "https://api.xivdb.com/data/exp_table",
+            "keys": recursive(EXPTABLE),
+        },
+
 }
 
 def databases():
